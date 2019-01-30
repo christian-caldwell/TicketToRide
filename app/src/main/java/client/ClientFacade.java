aacc4f26ce64949b51a7e6784f08416bce725991
Ben Knutson <bentknutson@gmail.com>
2019-01-29 21:31:35 -0700

    added client files

---
+++ app/src/main/java/client/ClientFacade.java
@@ -0,0 +1,18 @@
+package client;
+
+public class ClientFacade implements IClient {
+    @Override
+    public void updateClient() {
+
+    }
+
+    @Override
+    public void join() {
+
+    }
+
+    @Override
+    public void create() {
+
+    }
+}
---
+++ app/src/main/java/client/CommandManager.java
@@ -0,0 +1,18 @@
+package client;
+
+public class CommandManager implements IClient {
+    @Override
+    public void updateClient() {
+
+    }
+
+    @Override
+    public void join() {
+
+    }
+
+    @Override
+    public void create() {
+
+    }
+}
