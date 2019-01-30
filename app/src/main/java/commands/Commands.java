887b92a7f2d354d570687c389e4ffcab27c78355
Ben Knutson <bentknutson@gmail.com>
2019-01-29 21:38:04 -0700

    added command classes

---
+++ app/src/main/java/commands/Commands.java
@@ -0,0 +1,10 @@
+package commands;
+
+public class Commands {
+    private Commands _instance = new Commands();
+
+    public String create(params);
+    public String login(params);
+    public String join();
+    public String Register();
+}
---
+++ app/src/main/java/commands/GeneralCommand.java
@@ -0,0 +1,17 @@
+package commands;
+
+public class GeneralCommand implements ICommand{
+    private String _className;
+    private String _methodName;
+    private Class<?>[] _paramTypes;
+    private Object[] _paramValues;
+
+    //constructor
+    public GeneralCommand(String className, String methodName, Class<?>[] paramTypes, Object[] paramValues) {
+    }
+
+    @Override
+    public Result exec() {
+        return null;
+    }
+}
