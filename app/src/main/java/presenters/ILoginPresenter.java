1ed6432ea9c96d2bf7b0baf92c2072348f8d1e81
Zack Shorts <shorts23@gmail.com>
2019-02-03 20:10:23 -0700

    merged master into my branch

--- app/src/main/java/presenters/ILoginPresenter.java
+++ app/src/main/java/presenters/ILoginPresenter.java
@@ -3,4 +3,6 @@ package presenters;
 public interface ILoginPresenter {
 	public String registerUser(String username, String password, String repeatedPassword);
 	public String loginUser(String username, String password);
+
+	public void update();
 }
