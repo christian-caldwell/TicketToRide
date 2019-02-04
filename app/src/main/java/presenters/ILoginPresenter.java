81c058097cee8c7980b399b2f6563343e82afe67
christian-caldwell <christian.caldwell@hotmail.com>
2019-02-03 20:45:44 -0700

    Aligned IloginPresenter with master

--- app/src/main/java/presenters/ILoginPresenter.java
+++ app/src/main/java/presenters/ILoginPresenter.java
@@ -1,6 +1,8 @@
-package presenters;
+ackage presenters;
 
 public interface ILoginPresenter {
 	public String registerUser(String username, String password, String repeatedPassword);
 	public String loginUser(String username, String password);
+
+	public void update();
 }
