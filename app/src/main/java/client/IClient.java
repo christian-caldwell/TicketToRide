081398a2aa2570e0cf86e0bafd03f2ec95d13b6b
christian-caldwell <47019207+christian-caldwell@users.noreply.github.com>
2019-02-03 17:42:27 -0700

    Added Login Functions

--- app/src/main/java/client/IClient.java
+++ app/src/main/java/client/IClient.java
@@ -4,4 +4,7 @@ public interface IClient {
     public void updateClient();
     public void join();
     public void create();
+    public void updateAuthToken(String newAuthToken);
+    public String passAuthToken();
+    public void setUserValues(User newUser);
 }
