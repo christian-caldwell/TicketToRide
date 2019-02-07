a029a83566a9a5c4cdb7de45220df9411a36c786
christian-caldwell <christian.caldwell@hotmail.com>
2019-02-07 00:15:17 -0700

    Added lots of Classes to resemble new UML. Refactored a bunch of packages to improve clarity. Added some comments to pre-existing Classes

---
+++ app/src/main/java/view/activityInterface/IGameLobby.java
@@ -0,0 +1,10 @@
+package view.activityInterface;
+
+import java.util.Map;
+
+import models.data.Game;
+
+public interface IGameLobby {
+    void updateGameList(Map<String, Game> map);
+    void updateGamePlayers(Game game);
+}
