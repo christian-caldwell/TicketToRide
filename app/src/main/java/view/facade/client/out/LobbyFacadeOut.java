f5b8f815e62cb02290335090a87bc334bf694b34
christian-caldwell <christian.caldwell@hotmail.com>
2019-02-09 12:47:13 -0700

    worked on ServerProxy. ServerProxy lobby functions startGame and creatGame now properly generate a genericCommand and send it to the clientComm. instance. Updated IServer so that startGame returns Result instead of string.

--- app/src/main/java/view/facade/client/out/LobbyFacadeOut.java
+++ app/src/main/java/view/facade/client/out/LobbyFacadeOut.java
@@ -1,9 +1,23 @@
 package view.facade.client.out;
 
 import client.ServerProxy;
+import models.data.Request;
+import models.data.Result;
+import server.ServerCommands;
 
 public class LobbyFacadeOut {
+    private ServerCommands serverCommands;
 
-    private ServerProxy server = null;
+    public LobbyFacadeOut() {
+        serverCommands = new ServerCommands();
+    }
+
+    public Result createGame(Request request) {
+        return serverCommands.createGame(request.getGame().getGameName(), request.getUser());
+    }
+
+    public Result joinGame(Request request) {
+        return serverCommands.joinGame(request.getUser(), request.getGame());
+    }
 
 }
