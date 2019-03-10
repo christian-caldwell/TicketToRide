b8d7292b8ccd4483c88b50cb8cd1650a962862da
zackshorts <sHORT$32494>
2019-03-09 18:08:03 -0700

    added two more tests to demo, plus a bug fix.

--- app/src/main/java/view/presenter/DemoPresenter.java
+++ app/src/main/java/view/presenter/DemoPresenter.java
@@ -3,10 +3,14 @@ package view.presenter;
 import com.example.cs340.tickettoride.GameBoardActivity;
 import com.example.cs340.tickettoride.LoginViewActivity;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 import client.ServerProxy;
 import models.TTR_Constants;
+import models.data.ChatMessage;
 import models.data.DestinationCard;
 import models.data.Game;
 import models.data.Player;
@@ -35,6 +39,8 @@ public class DemoPresenter {
     private final RunGameFacade mRunGameFacade = new RunGameFacade();
     private final ClientFacade mClientFacade = new ClientFacade();
     private final ServerProxy mServerProxy = new ServerProxy();
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
+
 
     public DemoPresenter (LoginViewActivity loginActivity) {
         this.loginActivity = loginActivity;
@@ -327,10 +333,10 @@ public class DemoPresenter {
         cards2 = new DestinationCard[2];
         cards2[0] = player2.getDestinationCardHand().get(0);
         cards2[1] = player2.getDestinationCardHand().get(1);
-        mServerProxy.returnDestinationCards(christian.getUsername(), game1.getGameName(), cards1);
+        mServerProxy.returnDestinationCards(christian.getUsername(), game1.getGameName(), cards2);
         cards3 = new DestinationCard[2];
-        cards1[0] = player3.getDestinationCardHand().get(0);
-        cards1[1] = player3.getDestinationCardHand().get(1);
+        cards3[0] = player3.getDestinationCardHand().get(0);
+        cards3[1] = player3.getDestinationCardHand().get(1);
         mServerProxy.returnDestinationCards(ben.getUsername(), game1.getGameName(), cards1);
 
         try{
@@ -339,5 +345,47 @@ public class DemoPresenter {
             e.printStackTrace();
         }
 
+        //player 1 post chat message
+
+        game1 = mServerProxy.requestGame(game1.getGameName()).getRunningGame();
+        ChatMessage message = createMessage("Nice Demo!!!", player1.getUsername());
+        mServerProxy.postChatMessage(game1.getGameName(), message);
+
+        try{
+            Thread.sleep(3000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+        //post full convo
+
+        message = createMessage("Sup", player2.getUsername());
+        mServerProxy.postChatMessage(game1.getGameName(), message);
+        message = createMessage("Hey", player3.getUsername());
+        mServerProxy.postChatMessage(game1.getGameName(), message);
+        message = createMessage("Hello", player1.getUsername());
+        mServerProxy.postChatMessage(game1.getGameName(), message);
+        message = createMessage("I'm so cool", player2.getUsername());
+        mServerProxy.postChatMessage(game1.getGameName(), message);
+        message = createMessage("Absolutely", player3.getUsername());
+        mServerProxy.postChatMessage(game1.getGameName(), message);
+        message = createMessage("The coolest", player1.getUsername());
+        mServerProxy.postChatMessage(game1.getGameName(), message);
+        message = createMessage("Literaly the coolest ever", player3.getUsername());
+        mServerProxy.postChatMessage(game1.getGameName(), message);
+        message = createMessage("I am so glad this is a real conversation", player2.getUsername());
+        mServerProxy.postChatMessage(game1.getGameName(), message);
+
+
+    }
+
+    private ChatMessage createMessage (String contents, String userName) {
+        ChatMessage message = new ChatMessage();
+        message.setAuthorUserName(userName);
+        message.setMessageContents(contents);
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date.getTime());
+        message.setTimeStamp(sdf.format(timestamp));
+        return message;
     }
 }
