493ae859d525a6e2d54e9201471c51bc62d145a8
Zack Shorts <shorts23@gmail.com>
2019-03-21 19:33:53 -0600

    YourTurnSecondDraw now follows state pattern

--- app/src/main/java/client/PlayerStates/YourTurnDefault.java
+++ app/src/main/java/client/PlayerStates/YourTurnDefault.java
@@ -7,7 +7,6 @@ import models.data.Route;
 public class YourTurnDefault extends PlayerState {
     private static final YourTurnDefault ourInstance = new YourTurnDefault();
     ServerProxy serverProxy = new ServerProxy();
-
     public static YourTurnDefault getInstance() {
         return ourInstance;
     }
@@ -24,10 +23,13 @@ public class YourTurnDefault extends PlayerState {
         serverProxy.requestDestinationCards(clientModel.getUser().getUsername(), clientModel.getUser().getGame().getGameName());
         clientModel.setState(YourTurnAwaitingDestinations.getInstance());
     }
+
     public void purchaseRoute(ClientModel clientModel, Route route, int numberOfWilds){
         serverProxy.purchaseRoute(clientModel.getUser().getUsername(), clientModel.getUser().getGame().getGameName(), route, numberOfWilds);
         clientModel.setState(NotYourTurn.getInstance());
     }
+
     public void acceptPlayerAction(ClientModel clientModel){}
+
     public void leaveGame(ClientModel clientModel){}
 }
--- app/src/main/java/client/PlayerStates/YourTurnSecondDraw.java
+++ app/src/main/java/client/PlayerStates/YourTurnSecondDraw.java
@@ -1,12 +1,23 @@
 package client.PlayerStates;
 
+import client.ClientModel;
+import client.ServerProxy;
+
 public class YourTurnSecondDraw extends PlayerState {
     private static final YourTurnSecondDraw ourInstance = new YourTurnSecondDraw();
-
+    ServerProxy serverProxy = new ServerProxy();
     public static YourTurnSecondDraw getInstance() {
         return ourInstance;
     }
 
     private YourTurnSecondDraw() {
     }
+
+    public void requestTicketCard(ClientModel clientModel, int cardNum){
+        serverProxy.requestTicketCard(clientModel.getUser().getUsername(), clientModel.getUser().getGame().getGameName(), cardNum, false);
+        clientModel.setState(NotYourTurn.getInstance());
+    }
+
+    public void acceptPlayerAction(ClientModel clientModel){}
+    public void leaveGame(ClientModel clientModel){}
 }
